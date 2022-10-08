aebafd11eb467820218e1d215891d8a4ea68c9da
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-08 13:33:50 -0600

    css and html better, js time

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let input = getElementById("input")
+let clear = getElementById("clear")
+let equals = getElementById('equals')
+let parentheses = ['(',')']
+let operators = ['+','-','*','/','^']
+let numbers = ['0','1','2','3','4','5','6','7','8','9']
+
+
+let onClick = () => {
+}
+
+button.addEventListener("click", (e) => {
+	document.
+})
